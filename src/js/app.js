c15f23e1cb3ee34704ac8b9fc093c617ce1469aa
Konrad Litak <konrad.litakgit2502@gmail.com>
2024-06-18 19:07:57 +0100

    Add navigation

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,19 @@
-function test() {
-    console.log('test');
-}
+const arrowItem = document.querySelector('.navigation__dropdown-items--arrow');
+const asideContent = document.querySelector('.navigation__dropdown-content-aside');
 
-test();
+arrowItem.addEventListener('mouseenter', function() {
+    asideContent.style.display = 'flex';
+});
 
+arrowItem.addEventListener('mouseleave', function() {
+    asideContent.style.display = 'none';
+});
 
+asideContent.addEventListener('mouseenter', function() {
+    asideContent.style.display = 'flex';
+});
 
-
+asideContent.addEventListener('mouseleave', function() {
+    asideContent.style.display = 'none';
+});
 
